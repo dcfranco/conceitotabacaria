94c9d2b8779e9a02d533044d07385d5a4210a1fa
Daniel Carlos Franco <daniel.franco@linx.com.br>
2017-07-05 11:03:46 -0300

    GruposService finish

--- react/app/components/service/GruposService.jsx
+++ react/app/components/service/GruposService.jsx
@@ -1,12 +1,82 @@
-import {sqlConnection} from './mysql';
+import {sqlQuery} from './mysql';
 
 export default class GruposService {
-    static getGrupo(gru_codigo){
-        return new Promise((resolve, reject) => {
-            sqlConnection.query(`select * from tb_grupos where gru_codigo = ${gru_codigo}`, (error, results) => {                
-                if(error) reject(error);
-                else resolve(results);
-            });            
-        });
+    static async getGrupos(){
+        try {
+            let grupos = await sqlQuery('select * from tb_grupos', []);
+            return grupos;
+        } catch(e){
+            console.error(e);
+        }
+        return null;
+    }
+
+    static async getGrupo(gru_codigo){
+        try {
+            let grupo = await sqlQuery('select * from tb_grupos where gru_codigo = ?', [gru_codigo]);
+            return grupo;
+        } catch(e){
+            console.error(e);
+        }
+        return null;
+    }
+
+    static async postGrupo(grupo){
+        try {
+            let query = 'update tb_grupos set';
+            let params = [];
+            Object.keys(grupo).map((key) => {
+                if(key != 'gru_codigo'){
+	                query = query.concat(" ",`${key} = ?`);
+                    params.push(grupo[key]);
+                }                
+            });
+            query = query.concat(" ", 'where gru_codigo = ?');
+            params.push(grupo.gru_codigo);
+
+            let sucess = await sqlQuery(query, params);
+            return sucess;
+        } catch(e){
+            console.error(e);
+        }
+        return false;
+    }
+
+    static async putGrupo(grupo){
+        try {
+            let fields = "";
+            let values = "";
+            let params = [];
+
+            Object.keys(grupo).map((key, index) => {
+                if(key != 'gru_codigo'){
+                    if(index == 0) {
+	                    fields = fields.concat("",`${key}`);
+                        values = values.concat("",'?');
+                    } else {
+                        fields = fields.concat(", ",`${key}`);
+                        values = values.concat(", ",'?');
+                    }
+                    params.push(grupo[key]);
+                }                
+            });
+
+            let query = "insert into tb_grupos (".concat("", fields).concat(") values (", values).concat(")");
+            let sucess = await sqlQuery(query, params);
+            return sucess;
+        } catch(e){
+            console.error(e);
+        }
+        return false;
+    }
+
+    static async cutGrupo(grupo){
+        try {
+            let sucess = await sqlQuery("delete from tb_grupos where gru_codigo = ?", [grupo.gru_codigo]);
+            return sucess;
+        } catch(e){
+            console.error(e);
+        }
+        return false;
     }
 }
--- react/app/components/service/mysql.js
+++ react/app/components/service/mysql.js
@@ -1,8 +1,17 @@
 import mysql from 'mysql';
 
-export const sqlConnection = mysql.createConnection({
+const sqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'conceitotabacaria'
 });
+
+export const sqlQuery = (query, params) => {
+    return new Promise((resolve, reject) => {
+        sqlConnection.query(query, params, (error, results, fields) => {{
+            if(error) reject(error);
+            else resolve(results);
+        }});
+    });
+}
