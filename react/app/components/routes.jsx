e7ee5a5ad828f567184f8b9f742f3d678e771982
Daniel Carlos Franco <daniel.franco@linx.com.br>
2017-07-06 17:01:47 -0300

    Redux integration

--- react/app/components/routes.jsx
+++ react/app/components/routes.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import { Router, Route, IndexRedirect, hashHistory } from 'react-router'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { Router, Route, IndexRedirect, createMemoryHistory, hashHistory } from 'react-router'
 
 import {Estoque} from './estoque'
 import {App} from './App'
+import {reducers} from './reducers'
+
+const memoryHistory = createMemoryHistory();
+const store = createStore(reducers, undefined, applyMiddleware(thunk, routerMiddleware(hashHistory)));
 
 export const AppMain = (props) => (
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
-            <Route path='/estoque' component={Estoque} />
-            <Route path='/caixa' component={Caixa} />
-            <Route path='/balanco' component={Balanco} />
-            <IndexRedirect to='/estoque' />
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={memoryHistory}>
+            <Route path="/" component={App}>
+                <Route path='/estoque' component={Estoque} />
+                <Route path='/caixa' component={Caixa} />
+                <Route path='/balanco' component={Balanco} />
+                <IndexRedirect to='/estoque' />
+            </Route>
+        </Router>
+    </Provider>
 );
 
 const Caixa = (props) => (<div> Under construction </div>);
